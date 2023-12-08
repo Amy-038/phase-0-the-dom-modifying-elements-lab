3d2b8376387c031704b0d9bca73a648a95fd9a35
Amy-038 <amy.sirma@student.moringaschool.com>
2023-12-08 17:51:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Amy is the champion";
+
+document.body.append(newHeader);
+
+//newHeader.id = 'victory';
+
+//"h1".id = "victory"
